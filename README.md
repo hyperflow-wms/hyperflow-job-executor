c48d4b68a05d2e53603ae86f0465fed8e7cad659
Bartosz Balis <balis@agh.edu.pl>
2019-11-30 12:59:32 +0100

    Initial implementation

---
+++ README.md
@@ -0,0 +1,3 @@
+# HyperFlow Job executor
+
+This is a basic HyperFlow job executor that uses local directory path to read and write files, and Redis for communication with the HyperFlow engine.
---
+++ handler.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env node
+// Executor of 'jobs' using the Redis task status notification mechanism
+
+const { spawn } = require('child_process');
+
+var redis = require('redis');
+
+if (process.argv.length < 4) {
+  console.error("Usage: node index.js <taskId> <redis_url>");
+  process.exit(1);
+}
+
+// 'taskId' is the name of the Redis key (list) to use for the notification
+var taskId = process.argv[2],
+    redis_url = process.argv[3];
+
+//console.log("taskId", taskId);
+//console.log("redis_url", redis_url);
+
+var rcl = redis.createClient(redis_url);
+
+// get job message from Redis
+var getJobMessage = async function (timeout) {
+    return new Promise(function (resolve, reject) {
+        const jobMsgKey = taskId + "_msg";
+        rcl.brpop(jobMsgKey, timeout, function (err, reply) {
+            err ? reject(err): resolve(reply)
+        });
+    });
+}
+
+// send notification about job completion to Redis
+var notifyJobCompletion = async function () {
+    return new Promise(function (resolve, reject) {
+        rcl.rpush(taskId, "OK", function (err, reply) {
+            err ? reject(err): resolve(reply)
+        });
+    });
+}
+
+async function executeJob() {
+
+    // 1. Get job message
+    try {
+        var jobMessage = await getJobMessage(10);
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+    //console.log("Received job message:", jobMessage);
+
+    // 2. Execute job
+    var jm = JSON.parse(jobMessage[1]);
+
+    const cmd = spawn(jm["executable"], jm["args"]);
+
+    cmd.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    cmd.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    cmd.on('close', async(code) => {
+      console.log(`child process exited with code ${code}`);
+      // 3. Notify job completion
+      try {
+          await notifyJobCompletion();
+      } catch (err) {
+          console.error("Redis notification failed", err);
+          throw err;
+      }
+      process.exit(0);
+    });
+}
+
+executeJob()
