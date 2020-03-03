1cfa0a1c00fa66bc3d58c97c7977749939bf3cb2
Bartosz Balis <balis@agh.edu.pl>
2020-03-03 12:49:33 +0000

    Add logging process usage (cpu, memory)

--- handler.js
+++ handler.js
@@ -6,6 +6,8 @@ const redis = require('redis')
 const fs = require('fs')
 const log4js = require('log4js')
 const pidtree = require('pidtree')
+var pidusage = require('pidusage')
+
 
 const {
     procfs,
@@ -121,6 +123,24 @@ logProcInfo = function (pid) {
         }
     }
     logProcNetDev(pid);
+
+    logPidUsage = function(pid) {
+        pidusage(pid, function (err, stats) {
+            //console.log(stats);
+            // => {
+            //   cpu: 10.0,            // percentage (from 0 to 100*vcore)
+            //   memory: 357306368,    // bytes
+            //   ppid: 312,            // PPID
+            //   pid: 727,             // PID
+            //   ctime: 867000,        // ms user + system time
+            //   elapsed: 6650000,     // ms since the start of the process
+            //   timestamp: 864000000  // ms since epoch
+            // }
+            logger.info("Procusage: pid:", pid, JSON.stringify(stats));
+            setTimeout(() => logPidUsage(pid), 2000);
+        });
+    }
+    logPidUsage(pid);
 }
 
 // enable network IO monitoring using nethogs 
